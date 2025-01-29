90c9c8b58eedb88e92c79e23a23e286651f8a58d
ataurwd <ataurrahman24707@gmail.com>
2025-01-30 01:09:48 +0600

    ataur added

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-center text-4xl text-yellow-200'>welcome to our home page</h1>
+        </div>
+    );
+};
+
+export default Home;
